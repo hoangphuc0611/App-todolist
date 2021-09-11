24cf70b56e4cfb71894b83ca62fba7f9a314be55
hoangphuc0611 <53816838+hoangphuc0611@users.noreply.github.com>
2021-09-11 20:14:18 +0700

    huy_commit

--- backend/api/controllers/ProjectsController.js
+++ backend/api/controllers/ProjectsController.js
@@ -33,14 +33,11 @@ module.exports = {
   },
   updates: (req, res) => {
     let data = req.body["dataupdate"];
-    data.forEach(element => {
-      let sql = 'UPDATE Task SET ? WHERE project_ID = ?'
-      db.query(sql, [element, element["project_ID"]], (err, response) => {
-
-      })
-    })
-    res.json({ message: 'Update success!' })
-
+    data.forEach((element) => {
+      let sql = "UPDATE Task SET ? WHERE project_ID = ?";
+      db.query(sql, [element, element["project_ID"]], (err, response) => {});
+    });
+    res.json({ message: "Update success!" });
   },
   store: (req, res) => {
     let data = req.body;
@@ -48,11 +45,12 @@ module.exports = {
     db.query(sql, [data], (err, response) => {
       if (err) throw err;
     });
-    let sql1 = 'select * from Project having project_ID order by project_ID desc limit 1'
+    let sql1 =
+      "select * from Project having project_ID order by project_ID desc limit 1";
     db.query(sql1, (err, response) => {
-      if (err) throw err
-      res.json(response)
-    })
+      if (err) throw err;
+      res.json(response);
+    });
   },
   delete: (req, res) => {
     let sql = "DELETE FROM Project WHERE project_ID = ?";
--- backend/api/routes.js
+++ backend/api/routes.js
@@ -1,83 +1,70 @@
-'use strict';
-const jwt = require('jsonwebtoken')
-const util = require('util')
-const mysql = require('mysql')
-const db = require('./db')
-const { application } = require('express');
+"use strict";
+const jwt = require("jsonwebtoken");
+const util = require("util");
+const mysql = require("mysql");
+const db = require("./db");
+const { application } = require("express");
 
 module.exports = function (app) {
-  var projectsCtrl = require('./controllers/ProjectsController');
-  var messsCtrl = require('./controllers/MessagesController');
-  var taskCtrl = require('./controllers/TasksController');
-  var accCtrl = require('./controllers/AccsController');
-  var authenToken = require('./middleware/auth');
+  var projectsCtrl = require("./controllers/ProjectsController");
+  var messsCtrl = require("./controllers/MessagesController");
+  var taskCtrl = require("./controllers/TasksController");
+  var accCtrl = require("./controllers/AccsController");
+  var authenToken = require("./middleware/auth");
 
+  app.get("/projects", projectsCtrl.get);
 
-
-  app.get('/projects', projectsCtrl.get);
-
-  
-  app.post('/accs', accCtrl.store);
-  app.post('/login', (req, res) => {
+  app.post("/login", (req, res) => {
     const user = req.body;
     var accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: '10h'
+      expiresIn: "24h",
     });
 
-    let sql = 'SELECT * FROM Account_user where username=? and passwd=?'
+    let sql = "SELECT * FROM Account_user where username=? and passwd=?";
     var x = 0;
     db.query(sql, [user.username, user.passwd], (err, response) => {
-
       if (response.length) {
         res.json({ accessToken, response });
-        console.log(response[0])
-      }
-      else {
+        console.log(response[0]);
+      } else {
         res.status(403).send({
           message: "Sai taikhoan hoac matkhau",
         });
       }
     });
   });
+
+  app.get("/accs", accCtrl.get);
+  app.post("/accs", accCtrl.store);
+
   app.use(authenToken.isAuth);
 
-  app.get('/accs', accCtrl.get);
-  
+  app.post("/projects", projectsCtrl.store);
+  app.put("/projects", projectsCtrl.updates);
 
-  app.post('/projects', projectsCtrl.store);
-  app.put('/projects', projectsCtrl.updates);
-
-
-  app.route('/projects/:projectId')
+  app
+    .route("/projects/:projectId")
     .get(projectsCtrl.detail)
     .put(projectsCtrl.update)
     .delete(projectsCtrl.delete);
 
   // todoList Routes
 
-  
+  app.route("/messages").get(messsCtrl.get);
 
+  app.route("/messages/:projectId").get(messsCtrl.detail).post(messsCtrl.store);
 
-  app.route('/messages')
-    .get(messsCtrl.get)
+  app.route("/tasks").get(taskCtrl.get);
 
-  app.route('/messages/:projectId')
-    .get(messsCtrl.detail)
-    .post(messsCtrl.store);
-
-
-  app.route('/tasks')
-    .get(taskCtrl.get)
-
-
-  app.route('/tasks/:projectId')
+  app
+    .route("/tasks/:projectId")
     .get(taskCtrl.details)
     .post(taskCtrl.store)
     .put(taskCtrl.updates);
 
-  app.route('/tasks/:projectId/:taskId')
+  app
+    .route("/tasks/:projectId/:taskId")
     .get(taskCtrl.detail)
     .put(taskCtrl.update)
     .delete(taskCtrl.delete);
-
 };
